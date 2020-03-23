115e06e839f451882db29d6fc099799094b7cbd1
AlexanderIsraelsson <54535819+AlexanderIsraelsson@users.noreply.github.com>
2020-03-23 16:07:48 +0100

    Kan söka pokemon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const P = new Pokedex.Pokedex();
 
-P.getPokemonByName("bulbasaur")
+
+function readValue(){
+  var poke_dex = document.getElementById("Pokemon").value;
+  P.getPokemonByName(poke_dex)
   .then(res => {
     //om det blir rätt körs denna kod:
     console.log(res);
@@ -9,9 +12,13 @@ P.getPokemonByName("bulbasaur")
       "pokemon_sprite"
     ).innerHTML = `<img src="${res.sprites.front_default}"/>`;
   })
+
+  
   .catch(err => {
     //om det blir fel körs denna kod:
     console.log(err);
     document.getElementById("pokemon_name").innerHTML =
       "No pokemon by that name";
   });
+}
+
