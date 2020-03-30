a62e96ce5d4c59af68bcf8bf63a752fdbff7d774
AlexanderIsraelsson <54535819+AlexanderIsraelsson@users.noreply.github.com>
2020-03-30 16:15:38 +0200

    Fixade så att stats, types och moves skrivs ut.

--- js/script.js
+++ js/script.js
@@ -2,15 +2,26 @@ const P = new Pokedex.Pokedex();
 
 
 function readValue(){
-  var poke_dex = document.getElementById("Pokemon").value;
+  var poke_dex = document.getElementById("Pokemon").value.toLowerCase(); 
   P.getPokemonByName(poke_dex)
   .then(res => {
     //om det blir rätt körs denna kod:
     console.log(res);
     document.getElementById("pokemon_name").innerHTML = res.name;
     document.getElementById(
-      "pokemon_sprite"
-    ).innerHTML = `<img src="${res.sprites.front_default}"/>`;
+      "pokemon_sprite").innerHTML = `<img src="${res.sprites.front_default}"/>`;
+    document.getElementById("moves").innerHTML = "<b> Moves </b> <br>";
+    for(var i = 0; i < res.moves.length; i++){
+      document.getElementById("moves").innerHTML += res.moves[i].move.name + "<br>";
+    }
+    document.getElementById("stats").innerHTML = "<b> Stats </b> <br>";
+    for(var i = 0; i < res.stats.length; i++){
+      document.getElementById("stats").innerHTML += res.stats[i].stat.name + ": " + res.stats[i].base_stat + "<br>";
+    }
+    document.getElementById("types").innerHTML = "<b> Types </b> <br>";
+    for(var i = 0; i < res.types.length; i++){
+      document.getElementById("types").innerHTML += res.types[i].type.name + "<br>";
+    }
   })
 
   
@@ -21,4 +32,5 @@ function readValue(){
       "No pokemon by that name";
   });
 }
+
 
